ce64dc5ff6751a0a76038c96df730de4b65f6661
TheDonJinee <oma21007@byui.edu>
2022-12-14 22:26:00 +0100

    Project update

--- weatherapp/js/main.js
+++ weatherapp/js/main.js
@@ -23,27 +23,32 @@ async function fetchApi (city) {
 };
 
 function displayData(data) {
-    data = JSON.parse(localStorage.getItem('weather'));
-    const name = data.name;
-    const icon = data.weather[0].icon;
-    const description = data.weather[0].description;
-    const temp = data.main.temp;
-    const humidity = data.main.humidity;
-    const speed = data.wind.speed;
-    // console.log(name, icon, description, temp, speed, humidity)
-    // document.querySelector('.search-bar').value = localStorage.getItem(location);
-    document.querySelector('.city').innerText = `Weather in ${name}`;
-    document.querySelector('.icon').src = `http://openweathermap.org/img/wn/${icon}.png`;
-    document.querySelector('.icon').alt = `${description}`;
-    document.querySelector('.description').innerHTML = description;
-    document.querySelector('.temp').innerHTML = `${temp}&deg;C`;
-    document.querySelector('.humidity').innerHTML = `${humidity}%`;
-    document.querySelector('.wind').innerHTML = `${speed} km/h`;
-    document.querySelector('.weather').classList.remove("loading");
-    document.body.style.backgroundImage = `url('https://source.unsplash.com/800x534?${name}')`;
+    data = null || JSON.parse(localStorage.getItem('weather'));
+    if (data == null) {
+        console.log("Empty")
+    } else {
+        const name = data.name;
+        const icon = data.weather[0].icon;
+        const description = data.weather[0].description;
+        const temp = data.main.temp;
+        const humidity = data.main.humidity;
+        const speed = data.wind.speed;
+        // console.log(name, icon, description, temp, speed, humidity)
+        // document.querySelector('.search-bar').value = localStorage.getItem(location);
+        document.querySelector('.city').innerText = `Weather in ${name}`;
+        document.querySelector('.icon').src = `http://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector('.icon').alt = `${description}`;
+        document.querySelector('.description').innerHTML = description;
+        document.querySelector('.temp').innerHTML = `${temp}&deg;C`;
+        document.querySelector('.humidity').innerHTML = `${humidity}%`;
+        document.querySelector('.wind').innerHTML = `${speed} km/h`;
+        document.querySelector('.weather').classList.remove("loading");
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/800x534?${name}')`;
+    }
+
 }
 
-displayData(JSON.parse(localStorage.getItem('weather'))) 
+displayData([] || JSON.parse(localStorage.getItem('weather'))) 
 
 function search() {
     const city = searchInput.value;
